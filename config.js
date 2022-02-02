25d56c86dd42dd004d0dc85c71235ca76b1b79c7
Jordan Del Colle <jordandelcolle@Jordans-MacBook-Pro.local>
2022-02-01 19:01:50 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChIXvawFKwXlwLp1enjOLrx-Rd09UKJDM",
+  authDomain: "storefront-9e039.firebaseapp.com",
+  databaseURL: "https://storefront-9e039-default-rtdb.firebaseio.com",
+  projectId: "storefront-9e039",
+  storageBucket: "storefront-9e039.appspot.com",
+  messagingSenderId: "340618561184",
+  appId: "1:340618561184:web:3475788e735a1cebd0abc5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
